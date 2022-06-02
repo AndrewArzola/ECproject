6bcd55ebae22261187c1293dc57eb5ea7e5a56cf
AndrewArzola <andrew.j.arzola@gmail.com>
2022-06-02 09:44:43 -0500

    fix routes and added check and x marks

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,11 @@
 class House < ApplicationRecord
+    # validates :paid, inclusion: [true, false]
+
+    def plans_show(condtion)
+        if condtion
+        "\u2705"
+        else
+        "\u274C"
+        end
+    end
 end
